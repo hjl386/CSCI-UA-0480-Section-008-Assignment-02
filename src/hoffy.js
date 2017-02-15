6cdea2130f66019c3b9078227b3de04180e49a86
Han Jie Lee <hjl386@nyu.edu>
2017-02-15 18:04:32 -0500

    Working on simpleINIParse

--- src/hoffy.js
+++ src/hoffy.js
@@ -43,13 +43,70 @@ function maybe(fn){
 }
 
 function constrainDecorator(fn, min, max){
+	//Returns the value of the function unless the value is less than the min in which case it returns the min or the value is greater than the max and it returns the max value
+	return function(number){
+		/*if(min === undefined && max === undefined){
+			return Number(number);
+		} else if(number >= min && number <= max){
+			return Number(number);
+		} else if(number < min){
+			return min;
+		} else if(number > max){
+			return max;
+		}*/
+		return min === undefined && max === undefined ? Number(number) : number >= min && number <= max ? Number(number) : number < min ? min : max;
+	}
+}
 
+function limitCallsDecorator(fn, n){
+//Returns a function that does the same thing as the passed in function fn but can only be called n times.  After the nth call it does not return the original function, fn will not be called, and the return value will always be undefined.
+	let counter = 0; 
+	return function(...args){
+		if(counter < n) {
+			counter++;
+			return fn(...args);			
+		} else if(counter > n){
+			return undefined
+		}
+	}	
+}
+
+function mapWith(fn){
+//Returns a function that takes one parameter, an array, performs the function for every value in the array returning a new array
+	return function(arr){
+		let arrNew = arr.map(fn);
+		return arrNew;
+	}
+}
+
+function simpleINIParse(s){
+//Returns an object with the key-value pairs based on the passed in INI file ("foo=hi\nbar=hh\nbaz=gg', = separates the values and \n separates the pairs, if there is = then skip, if there is missing info foo= or =foo then assume ''). 
+	let object = {};
+	const str = s.split('\n');
+	let a = str.map(ele => {
+		return ele.split('=');		
+	});
+	console.log("The array A ", a);
+	object = a.reduce((ele, index) => {
+		console.log("The value you are working with ", a[index]);
+		console.log("Length at index ", index, "is ", a[index].length);
+		if(a[index].length === 2){
+			object.a[index][0] = a[index][i];
+		}
+	});
+	console.log("The Object ", object);
+	return object;
 }
 
 module.exports = {
 	prod: prod,
 	any: any,
 	maybe: maybe,
-	constrainDecorator: constrainDecorator
+	constrainDecorator: constrainDecorator,
+	limitCallsDecorator: limitCallsDecorator,
+	mapWith: mapWith,
+	simpleINIParse: simpleINIParse
 }
+
+
 
