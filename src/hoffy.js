f641aed762ea24b5a73c70eb60ad63a5e9cf355b
Han Jie Lee <hjl386@nyu.edu>
2017-02-15 10:52:31 -0500

    First 3 functions pass, working on constrainDecorator

--- src/hoffy.js
+++ src/hoffy.js
@@ -5,7 +5,6 @@ function prod(num1, ...numN){
 	let nums = [];
 	nums.push(num1);	
 	nums.push(...numN);
-//	console.log("array " + nums);
 	if(nums.length === 0){
 		return undefined;
 	} else if(nums.length === 1){
@@ -13,8 +12,7 @@ function prod(num1, ...numN){
 	} else {
 		const product = nums.reduce(function(acc, n){
 				return acc * n;		
-				}, 1);
-//		console.log(product);	
+				}, 1);	
 		return product;
 	}
 }
@@ -22,33 +20,36 @@ function prod(num1, ...numN){
 function any(arr, fn){
 //Returns true or false depending on whether or not any of the elements in the Array pass the test(fn).  Returns true as long as one test passes
 	const arrNew = arr.filter(fn);
-	if (arrNew.length > 0){return true;}
-	else {return false;}
+	return arrNew.length > 0 ? true : false;
 }
 
 function maybe(fn){
 //Takes in a function, calls it, and then returns its value unless it was passed in undefined or null parameters in which case it returns undefined
 	return function(...args){
 		let arr = [...args];
-		console.log(arr);
-		console.log(...args);
-		arr.filter(function(a){
-			if (a !== undefined || a !== null){
+		arr.filter(function(a, index){
+			if (a === undefined || a === null){
+				arr.splice(index, 1);
+			} else {
 				return a;
-			}
+			}	
 		});
-		console.log(arr);
 		if (arr.length !== [...args].length){
 			return undefined;
 		} else{
-			fn(...args);
+			return fn(...args);
 		} 	
 	}
+}
+
+function constrainDecorator(fn, min, max){
+
 }
 
 module.exports = {
 	prod: prod,
 	any: any,
-	maybe: maybe
+	maybe: maybe,
+	constrainDecorator: constrainDecorator
 }
 
