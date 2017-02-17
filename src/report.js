923fcb7221553573d9f3401fbff9af2970b068d4
Han Jie Lee <hjl386@nyu.edu>
2017-02-16 23:50:00 -0500

    Finished Homework 2, implementing recursion to visit all of the urls

--- src/report.js
+++ src/report.js
@@ -5,7 +5,7 @@ const basket = require('./basketfunc.js');
 const req = require('request');
 
 function readMyFile(url){
-	//Reads a JSON file and parses the data into a nested object of objects and arrays  
+//Reads a JSON file and parses the data into a nested object of objects and arrays  
 /*	fs.readFile('../tests/0021600681_gamedetail.json', 'utf8', (err, data) => {
 		if(err){
 			throw err;
@@ -17,31 +17,23 @@ function readMyFile(url){
 		console.log("\n\nSTR :", str);
 	});
 */
-	req('http://foureyes.github.io/csci-ua.0480-spring2017-008/homework/02/0021600680_gamedetail.json', function(error, response, body){
+//Reads data from URL
+//	req('http://foureyes.github.io/csci-ua.0480-spring2017-008/homework/02/0021600680_gamedetail.json', function(error, response, body){
+	req(url, function(error, response, body){
 		if(error){
 			console.log(error);	
 		}
-	//	console.log(body);
 		const str = JSON.parse(body);
-	//	console.log(str);
 		const a = basket.processGameData(str);
 		console.log(a);
-/*
-		while(str.g.hasOwnProperty('nextgid')){
-			let url = 'http://foureyes.github.io/csci-ua.0480-spring2017-008/homework/02/ '+str.g.nextgid + '_gamedetail.json';
-			req(url, function(error, response, body){
-				if(error){ console.log(error); }
-				const s = JSON.parse(body);
-				const b = basket.processGameData(s);
-				console.log(b);
-				let url = 'http://foureyes.github.io/csci-ua.0480-spring2017-008/homework/02/ '+ s.g.nextgid + '_gamedetail.json';	
-				console.log(s.g.nextgid);
-			});
-			//console.log(url);
-		}	
-*/
+		if(str.g.hasOwnProperty('nextgid')){
+			console.log("HHHHHH: ", str.g.nextgid);
+			const urlNew = 'http://foureyes.github.io/csci-ua.0480-spring2017-008/homework/02/'+str.g.nextgid + '_gamedetail.json';	
+			readMyFile(urlNew);
+		}
 	});
 }
 
-readMyFile();
+const url = 'http://foureyes.github.io/csci-ua.0480-spring2017-008/homework/02/0021600680_gamedetail.json';
+readMyFile(url);
 
