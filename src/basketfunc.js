354bb75ee7a717bf2f5ca8f348b555f5e70af26e
Han Jie Lee <hjl386@nyu.edu>
2017-02-16 22:52:08 -0500

    Basketfunc.js completed, eslint tests all passed

--- src/basketfunc.js
+++ src/basketfunc.js
@@ -1,19 +1,5 @@
 // basketfunc.js
 
-function processGameData(data){
-//Concatenoates all of the strings into the returned parsed string 
-	let rep = "";
-	const iDnDate = gameIDandDate(data);
-	rep += iDnDate;
-	const finalS = finalScoreStr(data);
-	rep += '\n' + finalS;
-	//console.log("\nREPORT\n" +  rep);
-	const rebs = mostRebounds(data);
-//	rep += '\n' + rebs;
-	console.log("REBS :", rebs);
-	return rep;	 
-}
-
 function gameIDandDate(data){
 //Returns a string with the "header", game id and date 
 	const str = "Game ID: " + data.g.gid + ", " + data.g.gdte + '\n=====';
@@ -22,10 +8,10 @@ function gameIDandDate(data){
 
 function finalScoreCalculate(data){
 	const scoreHls = data.g.hls.pstsg.reduce((acc, val, index) => {
-		return acc += ((data.g.hls.pstsg[index].tpm * 3) + (data.g.hls.pstsg[index].ftm) + ((data.g.hls.pstsg[index].fgm - data.g.hls.pstsg[index].tpm) * 2))  	
+		return acc += ((data.g.hls.pstsg[index].tpm * 3) + (data.g.hls.pstsg[index].ftm) + ((data.g.hls.pstsg[index].fgm - data.g.hls.pstsg[index].tpm) * 2));	
 	}, 0);
 	const scoreVls = data.g.vls.pstsg.reduce((acc, val, index) => {
-		return acc += ((data.g.vls.pstsg[index].tpm * 3) + (data.g.vls.pstsg[index].ftm) + ((data.g.vls.pstsg[index].fgm - data.g.vls.pstsg[index].tpm) * 2))  	
+		return acc += ((data.g.vls.pstsg[index].tpm * 3) + (data.g.vls.pstsg[index].ftm) + ((data.g.vls.pstsg[index].fgm - data.g.vls.pstsg[index].tpm) * 2)); 	
 
 	}, 0);
 	return [scoreHls, scoreVls];
@@ -34,8 +20,8 @@ function finalScoreCalculate(data){
 function finalScoreStr(data){
 //Returns a string with the calculated final score for both teams from the passed in data
 	const score = finalScoreCalculate(data); 
-	let teamHls = data.g.hls.tc + " " +  data.g.hls.tn + " - " + score[0];
-	let teamVls = data.g.vls.tc + " " +  data.g.vls.tn + " - " + score[1]; 
+	const teamHls = data.g.hls.tc + " " + data.g.hls.tn + " - " + score[0];
+	const teamVls = data.g.vls.tc + " " + data.g.vls.tn + " - " + score[1]; 
 	const str = teamHls + '\n' + teamVls;
 	return str;
 }
@@ -69,7 +55,7 @@ function mostRebounds(data){
 //Returns the str of the most rebounds
 	const rebH = mostReboundsHls(data);
 	const rebV = mostReboundsVls(data);
-	let reb = [];
+	const reb = [];
 	if(rebH[1] > rebV[1]){
 		reb.push(rebH[0]);
 		reb.push(rebH[1]);
@@ -80,6 +66,108 @@ function mostRebounds(data){
 	}
 	const str = "* Most rebounds:" + reb[0] + " with " + reb[1];
 	return str;
+}
+
+function threePointCal(data){ //Should have mapped this instead of filter it 
+//Calculates the threepoint
+	const tpHls = data.g.hls.pstsg.filter(function(ele){
+		return ele.tpa >= 5;
+	});
+	let base = parseInt(tpHls[0].tpm)/parseInt(tpHls[0].tpa); 
+	let name = tpHls[0].fn + " " + tpHls[0].ln;
+	const ratio = [];
+	tpHls.forEach(function(val){
+		if(parseInt(val.tpm)/parseInt(val.tpa) > base){
+			base = val.tpm/val.tpa;
+			name = val.fn + " " + val.ln;
+			ratio.push(val.tpm);
+			ratio.push(val.tpa);
+		}
+	});
+	const tpVls = data.g.vls.pstsg.filter(function(ele){
+		return ele.tpa >= 5;
+	});
+	let baseV = parseInt(tpVls[0].tpm)/parseInt(tpVls[0].tpa); 
+	let nameV = tpVls[0].fn + " " + tpVls[0].ln;
+	const ratioV = [];
+	tpVls.forEach(function(val){
+		if(parseInt(val.tpm)/parseInt(val.tpa) > base){
+			baseV = val.tpm/val.tpa;
+			nameV = val.fn + " " + val.ln;
+			ratioV.push(val.tpm);
+			ratioV.push(val.tpa);
+		}
+	});
+	return [name, base, nameV, baseV, ratio, ratioV];
+}
+
+function threePoint(data){
+//Returns the str of the threepoint data
+	const arr = threePointCal(data);
+	let base = 0;
+	let name = "";
+	let ratio = [];
+	if(arr[1] > arr[3]){
+		base = arr[1];
+		name = arr[0];
+		ratio = arr[4];
+	} else{
+		base = arr[3];
+		name = arr[2];
+		ratio = arr[5];
+	}
+	const str = "* Player with highest 3 point percentage that took at least 5 shots: " + name + " at %" + base*100 + " (" + ratio[0] + "/" + ratio[1] + ")";
+	return str;
+}
+
+function block(data){
+//Returns the str of block
+	const bH = data.g.hls.pstsg.filter(function(ele){
+		return ele.blk >= 1;
+	});
+	const bV = data.g.vls.pstsg.filter(function(ele){
+		return ele.blk >= 1;
+	});
+	const len = bH.length + bV.length;
+	const str = "* There were " + len + " players that had at least one block";
+	return str;
+}
+
+function turnAssists(data){
+//Returns the array of data with more turnovers than assists
+	const taH = data.g.hls.pstsg.filter(function(ele){
+		return ele.tov > ele.ast;
+	});
+	const taV = data.g.vls.pstsg.filter(function(ele){
+		return ele.tov > ele.ast;
+	});
+	return [taH, taV];
+}
+
+
+function processGameData(data){
+//Concatenoates all of the strings into the returned parsed string 
+	let rep = "";
+	const iDnDate = gameIDandDate(data);
+	rep += iDnDate;
+	const finalS = finalScoreStr(data);
+	rep += '\n' + finalS;
+	const rebs = mostRebounds(data);
+	rep += '\n' + rebs;
+	const tp = threePoint(data);
+	rep += '\n' + tp;
+	const blk = block(data);
+	rep += '\n' + blk;
+	rep += "\n* Players with more turnovers than assists:\n\t" + data.g.hls.tc + " - " + data.g.hls.tn + '\n'; 	
+	const t = turnAssists(data);
+	t[0].forEach(function(val){
+		rep += ("\t* " + val.fn + " " + val.ln + " has an assist to turnover ratio of " + val.ast + ":" + val.tov + '\n'); 
+	});
+	rep += "\n* Players with more turnovers than assists:\n\t" + data.g.vls.tc + " - " + data.g.vls.tn + '\n'; 		
+	t[1].forEach(function(val){
+		rep += ("\t* " + val.fn + " " + val.ln + " has an assist to turnover ratio of " + val.ast + ":" + val.tov + '\n'); 
+	});
+	return rep;	 
 }
 
 module.exports = {
