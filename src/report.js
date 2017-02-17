458e798a0ad1fadafa660658023c3edb4de9f41d
Han Jie Lee <hjl386@nyu.edu>
2017-02-16 22:58:58 -0500

    Working on URL input

--- src/report.js
+++ src/report.js
@@ -1,11 +1,12 @@
 //report.js
 
-const fs = require('fs');
+//const fs = require('fs');
 const basket = require('./basketfunc.js');
+const req = require('request');
 
 function readMyFile(){
 	//Reads a JSON file and parses the data into a nested object of objects and arrays  
-	fs.readFile('../tests/0021600681_gamedetail.json', 'utf8', (err, data) => {
+/*	fs.readFile('../tests/0021600681_gamedetail.json', 'utf8', (err, data) => {
 		if(err){
 			throw err;
 		}
@@ -14,6 +15,13 @@ function readMyFile(){
 		console.log(obj);
 		const str = basket.processGameData(obj);
 		console.log("\n\nSTR :", str);
+	});
+*/
+	req.request('http://foureyes.github.io/csci-ua.0480-spring2017-008/homework/02/0021600680_gamedetail.json', function(error, response, body){
+		if(error){
+			throw error;	
+		}
+		console.log(body);
 	});
 }
 
